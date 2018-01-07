7c7057aff41551ca4068ebcfb839204f802ba7fe
hanam1ni <immicky.a@gmail.com>
2018-01-08 00:46:12 +0700

    Edit Textbox to receive prop

--- src/components/molecules/Textbox.js
+++ src/components/molecules/Textbox.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
 
 export default class Textbox extends Component {
     render() {
+        const { title, children } = this.props
         return <div className="textbox-container">
-            <div className="textbox-title">
-                Remarks
-            </div>
+            { title && <div className="textbox-title">{title}</div> }
             <div className="textbox-content">
-                <p>When you enter into any new area of science, you almost always find yourself with a baffling new language of technical terms to learn before you can converse with the experts. This is certainly true in astronomy both in terms of terms that refer to the cosmos and terms that describe the tools of the trade, the most prevalent being the telescope. So to get us off of first base, let’s define some of the key terms that pertain to telescopes to help you be able to talk to them more intelligently. </p>
+                <p>{children}</p>
             </div>
         </div>
     }
+}
+
+Textbox.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.string,
+}
+
+Textbox.defaultProps = {
+    title: null,
+    children: null
 }
