21749361178b357b5bfaf6593a10a312d32daaaa
hanam1ni <immicky.a@gmail.com>
2018-01-07 22:55:04 +0700

    Add OrderHeader molecule in OrderHeaderSelect Part

--- src/components/molecules/OrderHeader.js
+++ src/components/molecules/OrderHeader.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { DropdownButton } from 'react-bootstrap'
 
 export default class OrderHeader extends Component {
     render() {
@@ -11,7 +12,36 @@ export default class OrderHeader extends Component {
                 <p>+66 087 348 79 34</p>
             </div>
             <div className="order-header-select">
-
+                <div>
+                    <span>Type</span>
+                </div>
+                <div>
+                    <DropdownButton
+                        title="Retailer"
+                        className="dropdownButton-default order-dropdown"
+                    >
+                    </DropdownButton>
+                </div>
+                <div>
+                    <span>Owned By</span>
+                </div>
+                <div>
+                    <DropdownButton
+                        title="Tom Hank"
+                        className="dropdownButton-default order-dropdown"
+                    >
+                    </DropdownButton>
+                </div>
+                <div>
+                    <span>Status</span>
+                </div>
+                <div>
+                    <DropdownButton
+                        title="Pending"
+                        className="dropdownButton-default order-dropdown"
+                    >
+                    </DropdownButton>
+                </div>
             </div>
         </div>
     }
