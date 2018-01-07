1d3b572613f857c8d6f42fc14ffee62ac3958310
hanam1ni <immicky.a@gmail.com>
2018-01-08 00:18:41 +0700

    Add knob to Breadcrumb in Storybook

--- stories/molecules.js
+++ stories/molecules.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
-import { withKnobs, object } from '@storybook/addon-knobs/react';
+import { withKnobs, object, array } from '@storybook/addon-knobs/react';
 
 import OrderHeader from 'molecules/OrderHeader.js'
 import Breadcrumb from 'molecules/Breadcrumb.js'
@@ -26,7 +26,8 @@ export default () => {
             return <OrderHeader detail={orderHeaderDetail} selection={orderHeaderSelect}></OrderHeader>
         })
         .add('Breadcrumb', () => {
-            return <Breadcrumb />
+            const levels = array('Level', ['Order', 'TN SO 000015'])
+            return <Breadcrumb levels={levels} />
         })
 }
  
