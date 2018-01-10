d27770482e30e2b769ffb42ab5eacdbc9a5c0c22
hanam1ni <immicky.a@gmail.com>
2018-01-11 00:11:28 +0700

    Change default active item to be null

--- src/components/molecules/SideMenu.js
+++ src/components/molecules/SideMenu.js
@@ -4,7 +4,7 @@ export default class SideMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            itemActive: 2,
+            itemActive: null,
         };
     }
 
