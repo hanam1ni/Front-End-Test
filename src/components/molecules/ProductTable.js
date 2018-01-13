9778af5d6d57f2444d830076518b0eb894d6988b
hanam1ni <immicky.a@gmail.com>
2018-01-13 12:36:38 +0700

    Edit table to support SetPrice Link

--- src/components/molecules/ProductTable.js
+++ src/components/molecules/ProductTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { isNil } from 'lodash'
 import { Table } from 'react-bootstrap'
 
 export default class ProductTable extends Component {
@@ -56,11 +57,9 @@ export default class ProductTable extends Component {
                 sku: 'TN SO 000015',
                 description: 'Magnetic contactor - 9A (5.5 kW\, 7.5 HP)\, control voltage 230 Vac',
                 brand: 'Schneider Electric',
-                listPrice: 'THB 880',
                 discount: '30%',
                 netPrice: 'THB 660',
                 qty: '16',
-                total: 'THB 50,079,8823'
             }
         ]
 
@@ -71,16 +70,31 @@ export default class ProductTable extends Component {
 
         const TableData = () => {
             const tableData = []
-            const columnOrder = mockColumn.map((column) => (column.data))
             mockData.map((data) => {
-                const dataKeys = Object.keys(data)
-                const dataCells = []
-                columnOrder.map((order) => {
-                    const cellClass = `table-data table-data-${order}`
-                    if (dataKeys.includes(order)) dataCells.push(<td className={cellClass}>{data[order]}</td>)
-                })
-                dataCells.push(<td className="table-data table-data-last"><a>Remove Item</a></td>)
-                tableData.push(<tr>{dataCells}</tr>)
+                const { sku, description, brand, listPrice, discount, netPrice, qty, total } = data
+                const dataRow = []
+                // SKU Cell
+                dataRow.push(<td className="table-data table-data-sku">{sku}</td>)
+                // Description Cell
+                dataRow.push(<td className="table-data table-data-description">{description}</td>)
+                // Brand Cell
+                dataRow.push(<td className="table-data table-data-brand">{brand}</td>)
+                // List Price Cell
+                isNil(listPrice) ? dataRow.push(<td className="table-data table-data-listPrice"><a>Set Price</a></td>)
+                : dataRow.push(<td className="table-data table-data-listPrice">{listPrice}</td>)
+                // Discount Cell
+                dataRow.push(<td className="table-data table-data-discount">{discount}</td>)
+                // Net Price Cell
+                isNil(netPrice) ? dataRow.push(<td className="table-data table-data-netPrice"><a>Set Price</a></td>)
+                : dataRow.push(<td className="table-data table-data-netPrice">{netPrice}</td>)
+                // Quantity Cell
+                dataRow.push(<td className="table-data table-data-qty">{qty}</td>)
+                // Total Cell
+                isNil(total) ? dataRow.push(<td className="table-data table-data-total"><a>Set Price</a></td>)
+                : dataRow.push(<td className="table-data table-data-total">{total}</td>)
+                // Remove Item Cell
+                dataRow.push(<td className="table-data table-data-last"><a>Remove Item</a></td>)
+                tableData.push(<tr>{dataRow}</tr>)
             })
             return tableData
         }
