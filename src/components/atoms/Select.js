79d07bf011317ac10e9da8fd93dec69f385fe49b
HanaM1ni <immicky.a@gmail.com>
2018-01-14 22:45:48 +0700

    Add custom arrow in Select atom

--- src/components/atoms/Select.js
+++ src/components/atoms/Select.js
@@ -4,11 +4,14 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 export default class Select extends Component {
     render () {
         return (
-            <FormControl componentClass="select" className="select-default" defaultValue="second">
-				<option value="selects">Selects</option>
-				<option value="second">Second</option>
-                <option value="other">...</option>
-			</FormControl>
+            <div className="select-container">
+                <FormControl componentClass="select" className="select-default" defaultValue="second">
+                    <option value="selects">Selects</option>
+                    <option value="second">Second</option>
+                    <option value="other">...</option>
+                </FormControl>
+                <div class="select-arrow"></div>
+            </div>
         )
     }
 }
