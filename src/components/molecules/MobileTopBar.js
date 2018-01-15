68f0916ab4d4da876bff451d9eaaaffb988ae482
HanaM1ni <immicky.a@gmail.com>
2018-01-16 00:15:09 +0700

    Add link to back and search icon

--- src/components/molecules/MobileTopBar.js
+++ src/components/molecules/MobileTopBar.js
@@ -4,7 +4,7 @@ export default class MobileTopBar extends Component {
     render () {
         return (
             <div className="mobile-top-bar-container">
-                <a onClick={() => console.log('Hello')}><img src="icons/arrow-orange.svg" className="back-icon"/></a>
+                <a><img src="icons/arrow-orange.svg" className="back-icon"/></a>
                 Edit ORD07070707
                 <a><img src="icons/magnifying-glass.svg" className="magnify-icon"/></a>
             </div>
