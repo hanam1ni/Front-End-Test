06cc3c421dc905ea3baf6db21565d72c1c102885
HanaM1ni <immicky.a@gmail.com>
2018-01-15 22:46:32 +0700

    Setup project for responsive

--- src/containers/OrderPage.js
+++ src/containers/OrderPage.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import Sidebar from 'organisms/Sidebar.js'
 import EditOrderSubPage from 'organisms/EditOrderSubpage.js'
+import Responsive from 'react-responsive'
 
 export default class OrderPage extends Component {
     render() {
+        const Desktop = (props) => (<Responsive {...props} minWidth={768} />)
+        const Mobile = (props) => (<Responsive {...props} maxWidth={767} />)
         return (
-            <div className="order-page-container">
-                <div className="side-bar-container">
-                    <Sidebar initialActive={1} />
-                </div>
-                <div className="sub-page-container">
-                    <EditOrderSubPage />
-                </div>
+            <div>
+                <Desktop>
+                    <div className="order-page-container">
+                        <div className="side-bar-container">
+                            <Sidebar initialActive={1} />
+                        </div>
+                        <div className="sub-page-container">
+                            <EditOrderSubPage />
+                        </div>
+                    </div>
+                </Desktop>
+                <Mobile>
+                    
+                </Mobile>
             </div>
         )
     }
