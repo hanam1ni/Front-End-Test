0da77701fc35912e188735c36893e67d91bce3f8
HanaM1ni <immicky.a@gmail.com>
2018-01-17 23:02:23 +0700

    Rotate icon when AddProductInput focus

--- src/components/molecules/AddProductInput.js
+++ src/components/molecules/AddProductInput.js
@@ -4,16 +4,31 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 import addIcon from 'statics/icons/icon_plus.svg'
 
 export default class AddProductInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            formFocus: false,
+        };
+    }
+
     render () {
+        const { formFocus } = this.state
         const { stretch, placeholder } = this.props
         const InputGroupClass = [
             "add-product-input",
             stretch ? "add-product-input-stretch" : null
         ].join(' ')
-        return <InputGroup className={InputGroupClass}>
-            <img role="img" src={addIcon} />
-            <FormControl type="text" placeholder={placeholder}/>
-        </InputGroup>
+        const toggleFocus = () => {
+            this.setState({
+                formFocus: !(this.state.formFocus)
+            })
+        }
+        return (
+            <InputGroup className={InputGroupClass}>
+                <img role="img" src={addIcon} className={formFocus ? "add-icon active" : "add-icon"} />
+                <FormControl type="text" placeholder={placeholder} onFocus={() => toggleFocus()} onBlur={() => toggleFocus()}/>
+            </InputGroup>
+        )
     }
 }
 
