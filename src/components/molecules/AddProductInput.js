c3c158f90eb267a83c3e53281007d78b2ea9870c
hanam1ni <immicky.a@gmail.com>
2018-01-18 23:42:17 +0700

    Add state for onChange timeout event in AddProductInput

--- src/components/molecules/AddProductInput.js
+++ src/components/molecules/AddProductInput.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'; 
 import { InputGroup, FormControl } from 'react-bootstrap';
 import addIcon from 'statics/icons/icon_plus.svg'
+import { debounce } from 'lodash';
 
 export default class AddProductInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
             formFocus: false,
+            formOnChange: false,
         };
+        this.handleOnChangeTimeout = debounce(() => { 
+            this.setState({
+                formOnChange: false
+            })
+        }, 400)
     }
 
     handleOnChange() {
-        setTimeout(() => {
-            console.log("Hello")
-        }, 500);
+        this.setState({
+            formOnChange: true
+        })
+        this.handleOnChangeTimeout()
     }
 
     handleToggleFocus() {
